21aa7f241d5a81f123bffa0d8cebdf7367b07ffc
Matheus Kisiolar <matheuskisiolar@gmail.com>
2023-02-12 19:48:58 -0300

    add simple ui and 2 number operations

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+const display = document.querySelector("#display");
+let displayValue = "";
+const number = [];
+const operation = [];
+
+
+document.querySelectorAll(".digit").forEach(x => x.addEventListener("click", type));
+document.querySelectorAll(".operator").forEach(x => x.addEventListener("click", type));
+document.querySelectorAll(".operator").forEach(x => x.addEventListener("click", pushToOperation));
+document.querySelector("#equal").addEventListener("click", createOperation);
+
+function type(e){
+    if (e.target.className === "digit") number.push(e.target.value);
+    displayValue += e.target.value;
+    display.textContent = displayValue;
+}
+
+function pushToOperation(e) {
+    operation.push(+number.join(""));
+    number.splice(0, number.length);
+    operation.push(e.target.value);
+}
+
+function createOperation() {
+    operation.push(+number.join(""));
+    number.splice(0, number.length);
+    display.textContent = operate(operation[0], operation[2], operation[1]);
+    operation.splice(0, operation.length);
+    displayValue = "";
+}
+
 function add(a, b) {
     return a + b;
 }
